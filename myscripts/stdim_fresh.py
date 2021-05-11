9a64326039ab3769bd4c3b8f1c2ea272ab100981
Md Mahfuzur Rahman <mahfuz.gsu@gmail.com>
2021-05-11 17:10:44 -0400

    Updating stdim revised version

--- myscripts/stdim_fresh.py
+++ myscripts/stdim_fresh.py
@@ -93,10 +93,16 @@ class LSTM(torch.nn.Module):
 
         self.encoder = encoder
         
+#         self.attn = nn.Sequential(
+#             nn.Linear(2*self.hidden_dim, 128),
+#             nn.ReLU(),
+#             nn.Linear(128, 1)
+#         )
+        
         self.decoder = nn.Sequential(
             nn.Linear(self.hidden_dim, 200),
             nn.ReLU(),
-            nn.Linear(200, output_size),
+            nn.Linear(200, output_size), 
             nn.Sigmoid()
         )
         
@@ -113,10 +119,43 @@ class LSTM(torch.nn.Module):
         for name, param in self.lstm.named_parameters():
             if 'weight' in name:
                 nn.init.xavier_normal_(param, gain=self.gain)
+                
+#         for name, param in self.attn.named_parameters():
+#             if 'weight' in name:
+#                 nn.init.xavier_normal_(param, gain=self.gain)
+                
         for name, param in self.decoder.named_parameters():
             print('Initializing Decoder:', name)
             if 'weight' in name:
                 nn.init.xavier_normal_(param, gain=self.gain)
+                
+    def add_regularization(self, loss):
+        
+#         print('Adding regularizer...')
+        reg = 1e-3
+        l2_reg = torch.tensor(0.).to(device)
+        
+        for name, param in self.encoder.named_parameters():
+            if 'bias' not in name:
+                l2_reg += torch.norm(param)
+                
+        for name, param in self.lstm.named_parameters():
+            if 'bias' not in name:
+                l2_reg += torch.norm(param)
+                
+        for name, param in self.decoder.named_parameters():
+            if 'bias' not in name:
+                l2_reg += torch.norm(param)
+                
+               
+        return loss + reg*l2_reg
+                
+    def init_hidden(self, batch_size, device):
+        
+        h0 = Variable(torch.zeros(2, batch_size, self.hidden_dim // 2, device=device))
+        c0 = Variable(torch.zeros(2, batch_size, self.hidden_dim // 2, device=device))
+        
+        return (h0, c0)
 
     def forward(self, x):
         
@@ -136,15 +175,22 @@ class LSTM(torch.nn.Module):
         out = self.encoder(x)
         
         out = out.view(b_size, s_size, -1)
-        lstm_out, hidden = self.lstm(out)
         
-        outputs = [self.decoder(torch.cat((x[0, self.hidden_dim // 2:],
-                                           x[-1, :self.hidden_dim // 2]), 0)) for x in lstm_out]
+        self.hidden = self.init_hidden(b_size, device)
+        lstm_out, self.hidden = self.lstm(out, self.hidden)
+        
+        # Attention added on lstm top
+        
+#         lstm_out = self.get_attention(lstm_out)
+#         lstm_out = lstm_out.view(b_size, -1)
+        
+        outputs = [self.decoder(torch.cat((hid[0, self.hidden_dim // 2:],
+                                           hid[-1, :self.hidden_dim // 2]), 0)) for hid in lstm_out]
         
         outputs = torch.stack(outputs)
         
         
-        return outputs
+        return outputs # lstm_out # outputs
 
     def get_attention(self, outputs):
         
@@ -157,7 +203,7 @@ class LSTM(torch.nn.Module):
         # For attention calculation
         b_size = outputs2.size(0)
         # out = outputs.view([-1, self.hidden])
-        out = outputs2.reshape(-1, 2*self.hidden)
+        out = outputs2.reshape(-1, 2*self.hidden_dim)
 
         weights = self.attn(out)
         weights = weights.view(b_size, -1, 1)
@@ -214,22 +260,68 @@ class DataWithLabels(Dataset):
 
 
 def get_data_loader(X, Y, batch_size):
-    dataLoader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X, Y), batch_size=batch_size)
+    dataLoader = torch.utils.data.DataLoader(torch.utils.data.TensorDataset(X, Y), batch_size=batch_size, shuffle = True)
 
     return dataLoader
 
 
-def train_model(model, loader_train, loader_test, epochs, learning_rate):
+def update_lr(learning_rate0, epoch_num, decay_rate):
+    """
+    Calculates updated the learning rate using exponential weight decay.
+    
+    Arguments:
+    learning_rate0 -- Original learning rate. Scalar
+    epoch_num -- Epoch number. Integer
+    decay_rate -- Decay rate. Scalar
+
+    Returns:
+    learning_rate -- Updated learning rate. Scalar 
+    """
+    
+    learning_rate = (1./(1 + decay_rate * epoch_num)) * learning_rate0
+    
+    return learning_rate
+
+
+def schedule_lr_decay(optimizer, learning_rate0, epoch_num, decay_rate=1, time_interval=1000):
+    """
+    Calculates updated the learning rate using exponential weight decay.
+    
+    Arguments:
+    learning_rate -- Original learning rate. Scalar
+    epoch_num -- Epoch number. Integer.
+    decay_rate -- Decay rate. Scalar.
+    time_interval -- Number of epochs where you update the learning rate.
+
+    Returns:
+    learning_rate -- Updated learning rate. Scalar 
+    """
+    
+    if epoch_num % time_interval == 0:
+        learning_rate = (1./(1 + decay_rate * (epoch_num / time_interval)))*learning_rate0
+    else:
+        learning_rate = optimizer.param_groups[0]["lr"]
+    
+    return learning_rate
+
+
+def train_model(model, loader_train, loader_train_check, loader_test, epochs, learning_rate):
     loss = torch.nn.CrossEntropyLoss()
 
     optimizer = optim.Adam(model.parameters(), lr=learning_rate)
 
-    # model.cuda()
     model.to(device)
     
     scheduler = torch.optim.lr_scheduler.ReduceLROnPlateau(optimizer, 'min')
 
     for epoch in range(epochs):
+        
+        learn_rate = schedule_lr_decay(optimizer, learning_rate, epoch, decay_rate=1, time_interval = 100)
+        for param_group in optimizer.param_groups:
+            param_group["lr"] = learn_rate
+            
+        cur_lr = optimizer.param_groups[0]["lr"] 
+       
 
         for i, data in enumerate(loader_train):
             x, y = data
@@ -238,6 +330,10 @@ def train_model(model, loader_train, loader_test, epochs, learning_rate):
             outputs= model(x)
            
             l = loss(outputs, y)
+            
+            # Add regularizer
+            l = model.add_regularization(l)
+            
             _, preds = torch.max(outputs.data, 1)
             accuracy = (preds == y).sum().item()
             l.backward()
@@ -253,10 +349,22 @@ def train_model(model, loader_train, loader_test, epochs, learning_rate):
         y_scores = sig.detach()[:, 1]
         roc = roc_auc_score(y_test.to('cpu'), y_scores.to('cpu'))
         
-        print("epoch: " + str(epoch) + ", loss: " + str(l.detach().item()) +", test acc: " + str(accuracy / y_test.size(0)) + ", roc: " + str(roc))
+        x_train, y_train = next(iter(loader_train_check))
+        train_outputs = model(x_train)
+        _, train_preds = torch.max(train_outputs.data, 1)
+        train_accuracy = (train_preds == y_train).sum().item()
+        train_accuracy /= y_train.size(0)
         
-        test_loss = loss(outputs, y_test)
-        scheduler.step(test_loss)
+        train_sig = F.softmax(train_outputs, dim=1).to(device)
+        y_train_scores = train_sig.detach()[:, 1]
+        train_roc = roc_auc_score(y_train.to('cpu'), y_train_scores.to('cpu'))
+        
+        
+        
+        print("epoch: " + str(epoch) + ", loss: " + str(l.detach().item()) +", test acc: " + str(accuracy / y_test.size(0)) + ", roc: " + str(roc) +", train acc: " + str(train_accuracy) +" , train roc: " + str(train_roc)+", learn rate: "+ str(cur_lr))
+        
+#         test_loss = loss(outputs, y_test)
+#         scheduler.step(test_loss)
         
         
     return optimizer, accuracy / y_test.size(0), roc
@@ -283,13 +391,20 @@ Params_subj_distr = { 'FBIRN': [15, 25, 50, 75, 100], 'COBRE': [15, 25, 40], 'OA
 
 test_lim_per_class = { 'FBIRN': 32, 'COBRE': 16, 'OASIS': 32, 'ABIDE': 50}
 
+# key : value = window_shift : dict for different datasets
+sequence_size = { 10: {'FBIRN': 13, 'COBRE': 13, 'OASIS': 11, 'ABIDE': 13}, 1: {'FBIRN': 121, 'COBRE': 121, 'OASIS': 101, 'ABIDE': 121}}
+
 # We need to set gains for the ST-DIM found earlier by Usman.
 
-Params_best_gains = {10: {'FBIRN': {'NPT':[0.15, 0.25, 0.5, 0.9, 0.9], 'UFPT': [0.05, 0.05, 0.05, 0.05, 0.05]}, 'ABIDE': {'NPT': [0.4, 0.5, 0.75, 0.75, 0.45, 0.8], 'UFPT': [0.15, 0.2, 0.25, 0.2, 0.35, 0.35]}, 'OASIS': {'NPT': [0.25, 0.35, 0.75, 0.30, 0.4, 0.35], 'UFPT': [0.05, 0.15, 0.3, 0.35, 0.65, 0.65]}}, 20: {'COBRE': {'NPT': [0.25, 0.35, 0.75], 'UFPT': [0.05, 0.45, 0.65]}}}
+Params_best_gains = {10: {'FBIRN': {'NPT':[0.15, 0.25, 0.5, 0.9, 0.9], 'UFPT': [0.05, 0.05, 0.05, 0.05, 0.05]}, 'ABIDE': {'NPT': [0.4, 0.5, 0.75, 0.75, 0.45, 0.8], 'UFPT': [0.15, 0.2, 0.25, 0.2, 0.35, 0.35]}, 'OASIS': {'NPT': [0.25, 0.35, 0.75, 0.30, 0.4, 0.35], 'UFPT': [0.05, 0.15, 0.3, 0.35, 0.65, 0.65]}, 'COBRE': {'NPT': [0.25, 0.35, 0.75], 'UFPT': [0.05, 0.45, 0.65]}}, 20: {'COBRE': {'NPT': [0.25, 0.35, 0.75], 'UFPT': [0.05, 0.45, 0.65]}}}
                      
-mode = int(sys.argv[1])  # Choose FPT (0) / UFPT (1) / NPT (2)
-data= int(sys.argv[2])  # Choose FBIRN (0)/ COBRE (1) / OASIS (2) / ABIDE (3)
 
+data= int(sys.argv[1])  # Choose FBIRN (0)/ COBRE (1) / OASIS (2) / ABIDE (3)
+mode = int(sys.argv[2])  # Choose FPT (0) / UFPT (1) / NPT (2)
+window_shift = int(sys.argv[3])
+test_ID = int(sys.argv[4])
+                 
+lr = 0.0005
 
 finalData, FNC, all_labels = FNCDict[Dataset[data]]()
 
@@ -300,8 +415,7 @@ no_good_comp = 53
 sample_y = 20
 subjects = Params[Dataset[data]][0]
 tc = Params[Dataset[data]][1]
-samples_per_subject = 7 #13 # Params[Dataset[data]][2]
-window_shift = 20 #10 #1
+samples_per_subject = sequence_size[window_shift][Dataset[data]] # Params[Dataset[data]][2]
 
 AllData = np.zeros((subjects, samples_per_subject, no_good_comp, sample_y))
 
@@ -320,14 +434,6 @@ test_starts = { 'FBIRN': [0, 32, 64, 96, 120], 'COBRE': [0, 16, 32, 48], 'OASIS'
 test_indices = test_starts[Dataset[data]]
 print(f'Test Start Indices: {test_indices}')
 
-# test_indices = [0, 32, 64, 96, 120]  # For FBIRN
-# test_indices = [0, 50, 100, 150, 200]   # For ABIDE
-# test_indices = [0, 32, 64, 96, 120, 152]  # For OASIS
-# test_indices = [0, 16, 32, 48] # For COBRE
-
-# for test_ID in range(1):
-
-test_ID = int(sys.argv[3])
 
 test_start_index = test_indices[test_ID]
 test_end_index = test_start_index + test_lim_per_class[Dataset[data]]
@@ -402,7 +508,7 @@ start_time = time.time()
 
 print(f'Allocated: {torch.cuda.memory_allocated()}')
 
-Best_gain = Params_best_gains[window_shift][Dataset[data]][MODELS[mode]]
+Best_gain = Params_best_gains[10][Dataset[data]][MODELS[mode]]
 print('Best Gain Chosen:', Best_gain)
 
 dir = MODELS[mode]   # NPT or UFPT
@@ -421,7 +527,7 @@ for i in range(len(subjects_per_group)):
 
         HC_random = trials_HC[restart][:samples]  
         SZ_random = trials_SZ[restart][:samples]
-
+        
 
         HC_index_tr = total_HC_index_tr[HC_random]
         SZ_index_tr = total_SZ_index_tr[SZ_random]
@@ -437,6 +543,23 @@ for i in range(len(subjects_per_group)):
 
         print('Train Data Shape:', X_train.shape)
         
+#         HC_val_random = trials_HC[restart][samples:]  
+#         SZ_val_random = trials_SZ[restart][samples:]
+        
+#         HC_index_val = total_HC_index_tr[HC_val_random]
+#         SZ_index_val = total_SZ_index_tr[SZ_val_random]
+        
+#         val_index = torch.cat((HC_index_val, SZ_index_val))
+#         val_index = val_index.view(val_index.size(0))
+#         X_val = X[val_index, :, :, :]
+#         Y_val = Y[val_index.long()]
+
+#         X_val = torch.from_numpy(X_val).float().to(device)
+#         Y_val = torch.from_numpy(Y_val).long().to(device)
+        
+#         dataLoaderVal = get_data_loader(X_val, Y_val, X_val.shape[0])
+
+        
         
         np.random.seed(0)
         randomize = np.random.permutation(X_train.shape[0])
@@ -445,6 +568,8 @@ for i in range(len(subjects_per_group)):
         Y_train_go = Y_train[randomize]
 
         dataLoaderTrain = get_data_loader(X_train_go, Y_train_go, 32)
+        
+        dataLoaderTrainCheck = get_data_loader(X_train_go, Y_train_go, X_train_go.shape[0])
         
         print(f'Test Split Starts: {test_ID}')
 
@@ -466,10 +591,10 @@ for i in range(len(subjects_per_group)):
 
         
         print(f'Allocated: {torch.cuda.memory_allocated()}')
-
+        
 
         model.to(device)
-        optimizer, accMat[i, restart], aucMat[i, restart] = train_model(model, dataLoaderTrain, dataLoaderTest, 200, .0005)
+        optimizer, accMat[i, restart], aucMat[i, restart] = train_model(model, dataLoaderTrain, dataLoaderTrainCheck, dataLoaderTest, 100, lr)
 
 #         dataLoaderFull = get_data_loader(X_sal, Y_sal, X_sal.shape[0])          
         dataLoaderSal = get_data_loader(X_sal, Y_sal, 1)
@@ -478,7 +603,7 @@ for i in range(len(subjects_per_group)):
         print('Total Time for Training:', middle_time)
 
 
-        prefix= f'{Dataset[data]}_spc_{subjects_per_group[i]}_simple_gain_{g}_window_shift_{window_shift}_{dir}_test_id_{test_ID}_STDIM'
+        prefix= f'{Dataset[data]}_spc_{subjects_per_group[i]}_simple_gain_{g}_window_shift_{window_shift}_{dir}_test_id_{test_ID}_STDIM_May9_lr_{lr}'
         
 #         prefix= f'{Dataset[data]}_spc_{subjects_per_group[i]}_gain_{g}_window_shift_{window_shift}_{dir}_arch_chk_LSTM_milc'
         
@@ -489,7 +614,7 @@ for i in range(len(subjects_per_group)):
         
 
         # Save model
-        save_reload_model(model, model_path, prefix, device, restart, save_reload = 'save')
+#         save_reload_model(model, model_path, prefix, device, restart, save_reload = 'save')
 
         if torch.cuda.is_available():
             
@@ -502,7 +627,7 @@ for i in range(len(subjects_per_group)):
         
 basename2 = os.path.join(sbpath, Directories[Dataset[data]], dir)
 
-prefix = f'{Dataset[data]}_all_spc_simple_val_best_gains_window_shift_{window_shift}_{dir}_test_id_{test_ID}_STDIM'
+prefix = f'{Dataset[data]}_all_spc_simple_val_best_gains_window_shift_{window_shift}_{dir}_test_id_{test_ID}_STDIM_May9_lr_{lr}'
 
 # prefix = f'{Dataset[data]}_all_spc_all_gains_{GAIN[ID]}_window_shift_{window_shift}_{dir}_STDIM'
 
